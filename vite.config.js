2184db01f7045a93d8f220eea75f96fd156604f2
Brandon Okoth <okothbrandon178@gmail.com>
2026-06-18 15:35:00 +0300

    updates

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,29 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor-react': ['react', 'react-dom', 'react-router-dom'],
+          'vendor-firebase': [
+            'firebase/app',
+            'firebase/auth',
+            'firebase/firestore',
+            'firebase/functions',
+            'firebase/storage',
+            'firebase/analytics',
+          ],
+          'vendor-fa': [
+            '@fortawesome/fontawesome-svg-core',
+            '@fortawesome/free-solid-svg-icons',
+            '@fortawesome/free-regular-svg-icons',
+            '@fortawesome/react-fontawesome',
+          ],
+        },
+      },
+    },
+  },
   server: {
     proxy: {
       '/api/financial-report': {
